80352bd87c5a6e19f4a22d3a3f4d9ccc6cd81eb6
sleeplesseditor <josh@bryson.me.uk>
2019-04-05 08:04:02 +0100

    Google Sheets API connection established

---
+++ src/components/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Container, Nav } from "./styled-components";
+
+import config from '../config';
+
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${config.spreadsheetId}/values:batchGet?ranges=Sheet1&majorDimension=ROWS&key=${config.apiKey}`;
+
+class App extends Component {
+  constructor() {
+    super();
+    this.statte = {
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        let batchRowValues = data.valueRanges[0].values;
+
+        const rows = [];
+        for (let i = 1; i < batchRowValues.length; i++) {
+          let rowObject = {};
+          for (let j = 0; j < batchRowValues[i].length; j++) {
+            rowObject[batchRowValues[0][j]] = batchRowValues[i][j];
+          }
+          rows.push(rowObject);
+        }
+        this.setState({ items: rows });
+    });
+  }
+
+
+  render() {
+    return (
+      <Container>
+        <Nav className="navbar navbar-expand-lg fixed-top is-white is-dark-text">
+          <div className="navbar-brand h1 mb-0 text-large font-medium">
+            Online Retail Dashboard
+          </div>
+          <div className="navbar-nav ml-auto">
+            <div className="user-detail-section">
+              <span className="pr-2">Hi, Sean</span>
+              <span className="img-container">
+                <img src="" className="rounded-circle" alt="user" />
+              </span>
+            </div>
+          </div>
+        </Nav>
+
+        <div className="container-fluid">
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default App;
